1c5c4cc22fbefe05dac12d1e7d5df41f4cca17b1
Salvador Moreno Tena <80414076+chavmoreno@users.noreply.github.com>
2023-09-11 09:42:05 -0600

    Update personal_functions.py

--- personal_functions.py
+++ personal_functions.py
@@ -15,3 +15,42 @@ def exportar_df_bq(df, dataset_tablename, gcp_project_name):
         project_id=gcp_project_name,
         if_exists="replace")
     print('\n The dataframe has been exported to the table \033[1m{}\033[0m in the proyect \033[1m{}\033[0m'.format(dataset_tablename, gcp_project_name))
+
+
+def getListOfFiles(google_drive, id_carpeta, fileExt = None, nombre_carpeta = None):
+    """
+    Return the id from Google Drive of all files inside a folder  (includes files from subfolders too).
+
+    Parameters:
+            google_drive (auth): Google drive authorization.
+            id_carpeta (str): Goolge id of the folder.
+            fileExt (str, optional): files extension.
+            nombre_carpeta (str, optional): name of the Google Drive's folder.
+
+    Returns:
+            allFiles (list): All the files from the folder listed.
+    """
+
+    file_list = google_drive.ListFile({'q': "'"+id_carpeta +"' in parents and trashed=false"}).GetList()
+    allFiles = list()
+    for entry in file_list:
+        if fileExt == None and entry['mimeType'] not in\
+        ['application/vnd.google-apps.folder',
+         'application/vnd.google-apps.document']:
+          fileExt = entry['fileExtension'].lower()
+        elif entry['mimeType'] == 'application/vnd.google-apps.document':
+          fileExt = 'doc'
+        arch_id = entry['id']
+        nombre = entry['title']
+        # Si el elemento es una carpeta, extrae los id's de sus archivos
+        if entry['mimeType'] == 'application/vnd.google-apps.folder':
+          allFiles = allFiles + getListOfFiles(google_drive,entry['id'],fileExt,nombre)
+          #allFiles.append(((nombre,arch_id,nombre_carpeta)))
+          if fileExt == None:
+            allFiles.append((nombre,arch_id,nombre_carpeta))
+        elif 'fileExtension' in entry.keys():
+          if entry['fileExtension'].lower() == fileExt.lower():
+            allFiles.append((nombre,arch_id,nombre_carpeta))
+
+    allFiles = list(set(allFiles))
+    return allFiles
